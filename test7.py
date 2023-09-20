678a1dee3a7c5a56fb8b707694e095b7d201194a
Suvrat Kumar Nanda <43516302+snanda2@users.noreply.github.com>
2023-09-20 19:42:20 +0530

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,6 +1,5 @@
-
-from jira import JIRA
-from datetime import date, datetime, timedelta
+import requests
+from datetime import datetime, timedelta
 import calendar
 
 # Jira server URL and credentials
@@ -11,20 +10,38 @@ PASSWORD = 'your_password'
 # Project key for the specific project you want to query
 PROJECT_KEY = 'YOUR_PROJECT_KEY'
 
-# Initialize Jira client
-jira = JIRA(server=JIRA_SERVER, basic_auth=(USERNAME, PASSWORD))
-
 # Calculate the start and end dates for the current month
-today = date.today()
+today = datetime.today()
 first_day_of_month = today.replace(day=1)
 last_day_of_month = today.replace(day=calendar.monthrange(today.year, today.month)[1])
 
-# Define JQL query to search for issues in the current month for the specified project
-jql_query = f'project = {PROJECT_KEY} AND created >= "{first_day_of_month}" AND created <= "{last_day_of_month}"'
+# Define the Jira API endpoint for searching issues
+api_endpoint = f'{JIRA_SERVER}/rest/api/2/search'
 
-# Execute the JQL query
-issues = jira.search_issues(jql_query)
+# Construct the JQL query for issues created in the current month for the specified project
+jql_query = f'project = {PROJECT_KEY} AND created >= "{first_day_of_month.strftime("%Y-%m-%d")}" AND created <= "{last_day_of_month.strftime("%Y-%m-%d")}"'
 
-# Print the issue keys and summaries
-for issue in issues:
-    print(f'Issue Key: {issue.key} - Summary: {issue.fields.summary}')
+# Set up headers for authentication
+headers = {
+    'Content-Type': 'application/json',
+}
+
+# Create a session for making authenticated requests
+session = requests.Session()
+session.auth = (USERNAME, PASSWORD)
+
+# Define the query parameters
+params = {
+    'jql': jql_query,
+}
+
+# Make the API request to Jira
+response = session.get(api_endpoint, headers=headers, params=params)
+
+# Check if the request was successful
+if response.status_code == 200:
+    issues = response.json().get('issues', [])
+    for issue in issues:
+        print(f'Issue Key: {issue["key"]} - Summary: {issue["fields"]["summary"]}')
+else:
+    print(f'Error: {response.status_code} - {response.text}')
